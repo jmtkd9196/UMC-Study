f707a0f1209531ae79e50b0dc7aa910a998e8139
KyungsooLee <jmtkd9196@gmail.com>
2023-05-10 14:26:06 +0900

    Create IOStagram Project

---
+++ iOStagram/iOStagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOStagram
+//
+//  Created by Kyungsoo Lee on 2023/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
